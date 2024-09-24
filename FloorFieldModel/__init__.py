8c977adad2c04f99afb7a2a01c47e2c36ea705da
SoraKurihara <120304128+SoraKurihara@users.noreply.github.com>
2024-09-24 14:31:05 +0900

    Update __init__.py

--- FloorFieldModel/__init__.py
+++ FloorFieldModel/__init__.py
@@ -1,7 +1,7 @@
-from cp import calc_probability
-from dcm import distance_calc_method
-from hc import handle_collision
-from sql import create_and_save_sqlite
+from .cp import calc_probability
+from .dcm import distance_calc_method
+from .hc import handle_collision
+from .sql import create_and_save_sqlite
 
 from .FFM import *
 
