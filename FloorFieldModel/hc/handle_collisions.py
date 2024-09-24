066a6fb197fd36e975b533e6dd289e4174e71917
SoraKurihara <120304128+SoraKurihara@users.noreply.github.com>
2024-09-24 12:21:10 +0900

    Update handle_collisions.py

--- FloorFieldModel/hc/handle_collisions.py
+++ FloorFieldModel/hc/handle_collisions.py
@@ -1,30 +1,4 @@
 import numpy as np
-
-
-# def handle_collisions(positions, candidates, unique_positions, counts):
-#     duplicates = unique_positions[counts > 1]
-
-#     # 重複している位置ごとの処理
-#     for dup in duplicates:
-#         # 位置が重複しているすべてのインデックスを取得
-#         # indices = np.where((candidates == dup).all(axis=1))[0]
-#         indices = np.where(np.sum(candidates == dup, axis=1) == candidates.shape[1])[0]
-
-#         # 1/2 の確率でアクションを選択
-#         if np.random.random() < 0.5:
-#             # 一人のみをその位置に残し、他のすべての人を元の位置に戻す
-#             chosen_one = np.random.choice(indices)
-#             for idx in indices:
-#                 if idx != chosen_one:
-#                     candidates[idx] = positions[idx]
-#         else:
-#             # すべての人を元の位置に戻す
-#             for idx in indices:
-#                 candidates[idx] = positions[idx]
-
-#     return candidates
-
-import numpy as np
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # 複数の重複を一度に処理する関数
